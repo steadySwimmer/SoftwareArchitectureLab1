a84c1c6bb85d9c4e3c21142d1138584c68146b30
SteadyCoder <iv.tkachenko17@gmail.com>
2017-04-03 22:36:46 +0300

    Create bussiness logic structure.

---
+++ bussiness_logic.py
@@ -0,0 +1,60 @@
+import datetime
+import pickle
+
+AGE_KEY = 'age'
+FIRSTNAME_KEY = 'firstname'
+
+BOOK_NAME_KEY = 'book_name'
+BOOK_AUTHOR_KEY = 'book_author'
+BOOK_YEAR_KEY = 'book_year'
+
+list_of_users = {}
+list_of_books = []
+
+
+def add_user(username, age, firstname):
+    pass
+
+
+def remove_user(username):
+    pass
+
+
+def add_book(book_name, book_author, book_year):
+    pass
+
+
+def remove_book(book_name):
+    pass
+
+
+def take_book(username):
+    pass
+
+
+def return_book(username):
+    pass
+
+
+def set_book_rate(book_name, rate):
+    pass
+
+
+def user_exist(username):
+    pass
+
+
+def book_exist(book_name):
+    pass
+
+
+def book_available(book_name):
+    pass
+
+
+def save_model(list_of_users, list_of_books):
+    pass
+
+
+def load_model():
+    pass
