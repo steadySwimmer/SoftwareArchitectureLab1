5e127fa3256a5bae1f618761708bf85467e18498
Vlad Podilnyk <vlad.podilnyk@gmail.com>
2017-04-07 15:13:06 +0300

    Add remove_book() method and override __str__() method.

--- User.py
+++ User.py
@@ -4,15 +4,15 @@ class User:
     """ Describes all properties of library user.
 
     Attributes:
-        __book_list(lst): contains books are taken by a particular user.
+        __book_list(lst): Contains books are taken by a particular user.
     """
 
     def __init__(self, user_name, age):
         """ Initialize User class.
 
         Args:
-            user_name(str): user name; can be an arbitrary sequence of characters.
-            age(int): user's age; must be in (0..100).
+            user_name(str): User name; can be an arbitrary sequence of characters.
+            age(int): User's age; must be in (0..100).
         """
         self.user_name = user_name
         self.age = age
@@ -21,12 +21,12 @@ class User:
 
     @property
     def user_name(self):
-        """ str: contains a name of a particular user """
+        """ str: Contains a name of a particular user """
         return self.__user_name
 
     @property
     def age(self):
-        """ int: contains an age of  a particular user, must be in range(1, 100) """
+        """ int: Contains an age of  a particular user, must be in range(1, 100) """
         return self.__age
 
     @age.setter
@@ -44,7 +44,19 @@ class User:
         """ Function add book to user's book list.
 
         Args:
-            book(:obj:Book): information about a book that user take.
+            book(:obj:Book): Information about a book that user take.
         """
         self.__book_list.append(book)
+
+    def return_book(self, title):
+        """ Remove book from the book list.
+
+        Args:
+            title(str): Book's title.
+        """
+        self.__book_list = [book for book in self.__book_list if book.book_name != title]
+
+    def __str__(self):
+        return "{}, age: {}".format(self.user_name, self.age)
+
 
